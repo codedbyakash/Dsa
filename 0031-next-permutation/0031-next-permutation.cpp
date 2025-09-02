df8afd71a96d9ad0d2357286a494a5b8a8b42964
Akash yadav <ay9514221@gmail.com>
2025-09-02 10:20:30 +0530

    Time: 0 ms (100%), Space: 15.5 MB (92.37%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+// reverse 
+
+// void reverse(vector<int>&nums,int i,int j){
+//     while(i<=j){
+//         int temp=nums[i];
+//         nums[i]=nums[j];
+//         nums[j]=temp;
+//         i++;
+//         j--;
+//     }
+//     return;
+
+    void nextPermutation(vector<int>& nums) {
+       int n=nums.size();
+       //finding pivot index
+       int idx=-1;
+       for(int i=n-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            idx=i;
+            break;
+        }
+       }
+        if(idx==-1){//if array is already greatest
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        // 2) sorting / reverse after pivot
+        reverse(nums.begin()+idx+1,nums.end());
+        //3), finding just greather number then index
+        int j=-1;
+       for(int i=idx+1;i<n;i++){
+        if(nums[i]>nums[idx]){
+            j=i;
+            break;
+        }
+       }
+
+        //  4) swapping idx and idx+1
+        int temp=nums[idx];
+        nums[idx]=nums[j];
+        nums[j]=temp;
+        return ;
+    }
+};
